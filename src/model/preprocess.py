547d4be0480800cebe61b027a39261e62cf40f00
TjanKazar <tjan.kazar3@gmail.com>
2026-06-05 20:44:43 +0200

    added verification

--- src/model/preprocess.py
+++ src/model/preprocess.py
@@ -325,7 +325,7 @@ if __name__ == "__main__":
 
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     mlflow.set_experiment(MLFLOW_EXPERIMENT)
-
+ 
     with mlflow.start_run(run_name="train_traffic_xgboost"):
 
         # Log preprocessing params
