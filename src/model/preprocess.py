ce979415600cccad9236b6787a7ec96fb2d4d37b
TjanKazar <tjan.kazar3@gmail.com>
2026-06-07 15:36:07 +0200

    api workflow fix

--- src/model/preprocess.py
+++ src/model/preprocess.py
@@ -24,7 +24,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from torch.utils.data import DataLoader, TensorDataset
 
-from src.model.lstm_utils import (
+from lstm_utils import (
     _predict,
     evaluate_chronos_mae,
     forecast_all_metrics,
@@ -32,7 +32,7 @@ from src.model.lstm_utils import (
     prepare_chronos_context,
     run_chronos_forecast,
 )
-from src.model.transforms import (
+from transforms import (
     CategoricalEncoder,
     DatePreprocessor,
     LagFeatureTransformer,
